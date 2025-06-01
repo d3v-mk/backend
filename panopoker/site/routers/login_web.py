fb8e24ecc5aebe3f6b7bbea3e17fcc0ae5abc0a4
Murilo Ferreira <devsoulmk@gmail.com>
2025-06-01 02:47:06 -0300

    debug

--- panopoker/site/routers/login_web.py
+++ panopoker/site/routers/login_web.py
@@ -55,7 +55,8 @@ def processar_login(request: Request, response: Response,
     value=token,
     httponly=True,
     secure=True,          # 🔒 obrigatório em HTTPS (ngrok é HTTPS)
-    samesite="None"       # 🔁 permite redirecionamento entre domínios
+    samesite="None",       # 🔁 permite redirecionamento entre domínios
+    domain=".panopoker.com"
     )
     return resp
 
