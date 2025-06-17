ba61e8162ae117f841b520c3530867818aa8803e
Murilo Ferreira <devsoulmk@gmail.com>
2025-06-16 22:21:20 -0300

    refactor: ajustando o main_web e extinguindo o old main_site

--- panopoker/main_web.py
+++ panopoker/main_web.py
@@ -3,8 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from panopoker.site.routers.listar_promotor import router as listar_promotor_router
 from panopoker.site.routers import login_web, painel_promotor, loja_promotor, configurar_loja, site_rank
 from panopoker.usuarios.routers import admin
+from panopoker.core.config import settings
 
-app_web = FastAPI()
+app_web = FastAPI(
+    docs_url="/docs" if not settings.IS_PRODUCTION else None,
+    redoc_url="/redoc" if not settings.IS_PRODUCTION else None,
+    openapi_url="/openapi.json" if not settings.IS_PRODUCTION else None,
+)
 
 app_web.add_middleware(
     CORSMiddleware,
@@ -21,7 +26,3 @@ app_web.include_router(painel_promotor.router)
 app_web.include_router(loja_promotor.router)
 app_web.include_router(configurar_loja.router)
 app_web.include_router(site_rank.router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
